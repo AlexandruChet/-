327ec3f84cc81d3dfa5c189eb38e2d2d1500843b
AlexandruChet <chetreanalexandru63@gmail.com>
2024-10-22 00:30:12 +0300

    update 3

--- project/src/js-(javascript)/app.js
+++ project/src/js-(javascript)/app.js
@@ -1,44 +1,60 @@
-let btnBlock = document.getElementById("btnBlock");
-let text = document.getElementById("text");
+let btnBlock = document.getElementById('btnBlock')
+let text = document.getElementById('text')
 
 btnBlock.onclick = function () {
 	if (text.textContent === '') {
 		text.textContent =
-			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur officia maxime excepturi dicta error accusamus totam quo. Eum corporis nulla aspernatur dolorum nisi, natus earum, recusandae illo, tenetur esse tempore?';
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur officia maxime excepturi dicta error accusamus totam quo. Eum corporis nulla aspernatur dolorum nisi, natus earum, recusandae illo, tenetur esse tempore?'
 	} else {
-		text.textContent = '';
+		text.textContent = ''
 	}
 }
 
-let img = document.getElementsByClassName("myImg")
-img.alt = "Якщо ви ще захочите зайти і купити чудові картини то пропоную залишитись на цьому сайті"
+let img = document.getElementsByClassName('myImg')
+img.alt =
+	'Якщо ви ще захочите зайти і купити чудові картини то пропоную залишитись на цьому сайті'
 
 function toggleAnswer(questionNumber) {
 	const answer = document.getElementById(`answer${questionNumber}`)
-	if (answer.style.display === "none" || answer.style.display === "") {
-		answer.style.display = "block"
-	}
-	else {
-		answer.style.display = "none"
+	if (answer.style.display === 'none' || answer.style.display === '') {
+		answer.style.display = 'block'
+	} else {
+		answer.style.display = 'none'
 	}
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("modalOverlay").style.display = 'block';
-});
+document.addEventListener('DOMContentLoaded', function () {
+	document.getElementById('modalOverlay').style.display = 'block'
+})
 
 function closeModal() {
-    document.getElementById('modalOverlay').style.display = 'none';
+	document.getElementById('modalOverlay').style.display = 'none'
 }
 
-let newBtn = document.getElementById("newBtn");
-let myNewText = document.getElementById("myNewtext");
+let newBtn = document.getElementById('newBtn')
+let myNewText = document.getElementById('myNewtext')
 
 newBtn.onclick = function () {
 	if (myNewText.textContent === '') {
 		myNewText.textContent =
-			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur officia maxime excepturi dicta error accusamus totam quo. Eum corporis nulla aspernatur dolorum nisi, natus earum, recusandae illo, tenetur esse tempore?';
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur officia maxime excepturi dicta error accusamus totam quo. Eum corporis nulla aspernatur dolorum nisi, natus earum, recusandae illo, tenetur esse tempore?'
 	} else {
-		myNewText.textContent = '';
+		myNewText.textContent = ''
 	}
 }
+
+const burgerMenu = document.getElementById('burger-menu')
+const navList = document.querySelector('.nav-list')
+
+burgerMenu.addEventListener('click', () => {
+	burgerMenu.classList.toggle('active')
+	navList.classList.toggle('active')
+})
+
+const callBtns = document.querySelectorAll('.callNumberBtn');
+
+callBtns.forEach(btn => {
+	btn.addEventListener('click', function() {
+		window.location.href = 'tel:+1234567890';
+	});
+});
